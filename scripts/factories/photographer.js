18e09e912d3235d34e63c5ab7619410c12dc69cd
ClementAver <clement.aver@yahoo.fr>
2023-01-04 14:15:23 +0100

    extends photographer.js factory pattern, adds Css rules for the users cards and an alt for the logo on index.html.

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,40 @@
 function photographerFactory(data) {
-  const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
+
+    const a = document.createElement("a");
     const img = document.createElement("img");
+    const userDescription = document.createElement("span");
+    userDescription.textContent = name;
+    userDescription.id = name.replace(/ /g, "");
+    userDescription.classList.add("sr-only");
+    a.setAttribute("aria-describedby", name.replace(/ /g, ""));
+    a.setAttribute("tabindex", "0");
+
     img.setAttribute("src", picture);
+    img.setAttribute("alt", "");
     const h2 = document.createElement("h2");
     h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+    const p1 = document.createElement("p");
+    p1.textContent = `${city}, ${country}`;
+    const p2 = document.createElement("p");
+    p2.textContent = tagline;
+    const p3 = document.createElement("p");
+    p3.textContent = `${price}€/jour`;
+
+    a.appendChild(userDescription);
+    a.appendChild(img);
+    a.appendChild(h2);
+
+    article.appendChild(a);
+    article.appendChild(p1);
+    article.appendChild(p2);
+    article.appendChild(p3);
+
     return article;
   }
   return { name, picture, getUserCardDOM };
